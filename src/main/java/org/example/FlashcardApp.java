2ed51d3b603ff785aeab6b2d4a4cda61ff0290f7
Nolan Geiger <nolan.geiger@gmail.com>
2023-04-26 12:15:09 -0400

    Changed the line deck.loadFlashcardsFromXMLFile to reflect changes in the file structure of the project.

--- src/main/java/org/example/FlashcardApp.java
+++ src/main/java/org/example/FlashcardApp.java
@@ -1,20 +1,38 @@
 package org.example;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class FlashcardApp {
+
     public static void main(String[] args) {
+        FlashcardDeck deck = new FlashcardDeck();
         try {
-            // Create a new FlashcardDeck object
-            FlashcardDeck deck = new FlashcardDeck();
-
-            // Load the flashcards from the XML file
-            deck.loadFlashcardsFromXmlFile("resources/flashcards.xml");
-
-            // Get the list of flashcards from the deck
-            List<Flashcard> flashcards = deck.getFlashcards();
+            deck.loadFlashcardsFromXmlFile("C:/Users/Nolan/IdeaProjects/Log4j-test/resources/flashcards.xml");
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.err.println("Error loading flashcards: " + e.getMessage());
+            System.exit(1);
         }
+        start(deck);
     }
+
+    public static void start(FlashcardDeck deck) {
+        List<Flashcard> flashcards = deck.getFlashcards();
+        Scanner scanner = new Scanner(System.in);
+        int numCorrect = 0;
+
+        for (Flashcard flashcard : flashcards) {
+            System.out.println(flashcard.getQuestion());
+            String answer = scanner.nextLine();
+            if (answer.equals(flashcard.getAnswer())) {
+                System.out.println("Correct!");
+                numCorrect++;
+            } else {
+                System.out.println("Incorrect. The correct answer is: " + flashcard.getAnswer());
+            }
+        }
+
+        System.out.println("Quiz complete. You answered " + numCorrect + " out of " + flashcards.size() + " correctly.");
+    }
+
 }
